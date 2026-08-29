77be0fdd0cbbec85d16ff3de6352671c090f8675
skt-pj <134942813+skt-pj@users.noreply.github.com>
2026-08-30 04:11:42 +0900

    Log character selection transition

--- app/src/main/java/com/sktpj/dynavorgmonsters/MainActivity.kt
+++ app/src/main/java/com/sktpj/dynavorgmonsters/MainActivity.kt
@@ -3,11 +3,16 @@ package com.sktpj.dynavorgmonsters
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 
 class MainActivity : Activity() {
+    private companion object {
+        const val TAG = "DVMSelection"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
@@ -24,13 +29,17 @@ class MainActivity : Activity() {
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             )
+        Log.i(TAG, "app_start version=${BuildConfig.VERSION_NAME}")
         showCharacterSelect()
     }
 
     private fun showCharacterSelect() {
         setContentView(
             CharacterSelectView(this) { player, enemy ->
-                setContentView(SpriteGameContainer(this, player, enemy))
+                Log.i(TAG, "battle_transition_begin player=${player.id} enemy=${enemy.id}")
+                val battleView = SpriteGameContainer(this, player, enemy)
+                setContentView(battleView)
+                Log.i(TAG, "battle_transition_complete player=${player.id} enemy=${enemy.id}")
             },
         )
     }
